e407f5fdd3a27179896c0fa78bd8cb323d1561c4
Julia-Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-05-26 19:15:20 -0300

    commit

---
+++ src/View/Main.java
@@ -0,0 +1,27 @@
+package View;
+
+import model.Arvore;
+
+public class Main {
+	public static void main(String[] args) {
+        int[] vetor = {33, 15, 41, 38, 47, 34, 49, 43};
+        Arvore arvore = new Arvore();
+        for (int i : vetor) {
+            arvore.insert(i);
+        }
+        try {
+            arvore.prefixSearch();
+            System.out.println();
+            arvore.infixSearch();
+            System.out.println();
+            arvore.postfixSearch();
+            System.out.println();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+        System.out.println();
+    }
+
+}
